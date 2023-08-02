c5152c388297f1b35436363d0e817c649e173fb5
Robin Hossain <robinhossain5261@gmail.com>
2023-08-02 16:21:46 +0600

    notification done

---
+++ src/app/(withlayout)/notification/page.js
@@ -0,0 +1,142 @@
+"use client"
+import React from 'react';
+import { latestNotifications, todayNotifications, yesterdayNotifications } from '../../../../public/data/utillityData';
+import Image from 'next/image';
+import { Tab } from '@headlessui/react';
+import { Fragment } from 'react';
+
+const Notification = () => {
+    return (
+        <div className='bg-bgColor dark:bg-darkBg px-6 pt-10 pb-6'>
+            <div className='flex items-center justify-center'>
+                <div className='px-8 py-7 bg-white dark:bg-darkCard rounded'>
+                    <Tab.Group>
+                        <Tab.List className='flex flex-wrap gap-2 justify-between items-center'>
+                            <h3 className='text-2xl sm:text-[32px] text-myBlack dark:text-myWhite font-semibold'>Notification center</h3>
+                            <div className='flex flex-wrap gap-6'>
+                                <Tab
+                                    className='w-24 text-sm text-black dark:text-myWhite py-3 px-4 cardShadow rounded cursor-pointer outline-none'
+                                    as={Fragment}>
+                                    {({ selected }) => (
+                                        <button
+                                            className={
+                                                selected ? 'bg-myBlue dark:bg-myGreen text-white' : 'text-myBlack dark:text-myWhite'
+                                            }
+                                        >
+                                            All
+                                        </button>
+                                    )}
+                                </Tab>
+                                <Tab
+                                    className='w-24 text-sm text-black dark:text-myWhite py-3 px-4 cardShadow rounded cursor-pointer outline-none'
+                                    as={Fragment}>
+                                    {({ selected }) => (
+                                        <div className={
+                                            selected ? 'bg-myBlue dark:bg-myGreen text-white flex' : ' text-myBlack dark:text-myWhite flex'
+                                        }
+                                        >
+                                            <button>Latest</button>
+                                            <div className='ml-3 h-[6px] w-[12px] rounded-full bg-[#FFA5A5] p-3 flex items-center justify-center'>
+                                                <span className='text-[8px]'>{latestNotifications.length}</span>
+                                            </div>
+                                        </div>
+                                    )}
+                                </Tab>
+                            </div>
+                        </Tab.List>
+                        <hr className='my-8 border-myLine dark:border-darkLine' />
+                        <Tab.Panels>
+                            <Tab.Panel>
+                                <section>
+                                    <h4 className='text-xl text-myBlack dark:text-white mb-6'>Today</h4>
+                                    <div>
+                                        {
+                                            todayNotifications.map(notification => (<div
+                                                key={notification.id}
+                                                className='flex flex-wrap gap-3  bg-bgColor dark:bg-[#1E192A] py-5 px-3 mb-2 rounded'
+                                            >
+                                                <Image className='w-[60px] h-[60px] rounded-full' src={notification.img} alt="people" />
+                                                <div>
+                                                    <div className='flex flex-wrap gap-2'>
+                                                        <h5 className='text-xl text-myBlack dark:text-white'>{notification.name}</h5>
+                                                        <span className='text-lg text-myGrey dark:text-myLine'>{notification.des}</span>
+                                                    </div>
+                                                    <div className='flex flex-wrap justify-between items-center'>
+                                                        <div className='flex items-center gap-1'>
+                                                            <span className='text-myBlack dark:text-myLine'>{notification.time}</span>
+                                                            <div className='w-[6px] h-[6px] bg-[#FFA5A5] rounded-full'></div>
+                                                        </div>
+                                                        <span className='text-xs text-myBlue dark:text-myGreen'>{notification.date}</span>
+                                                    </div>
+                                                </div>
+                                            </div>))
+                                        }
+                                    </div>
+                                </section>
+                                <section className='mt-[30px]'>
+                                    <h4 className='text-xl text-myBlack dark:text-myWhite mb-6'>Yesterday</h4>
+                                    <div>
+                                        {
+                                            yesterdayNotifications.map(notification => (<div
+                                                key={notification.id}
+                                                className='flex flex-wrap gap-3  bg-bgColor dark:bg-[#1E192A] py-5 px-3 mb-2 rounded'
+                                            >
+                                                <Image className='w-[60px] h-[60px] rounded-full' src={notification.img} alt="people" />
+                                                <div>
+                                                    <div className='flex flex-wrap gap-2'>
+                                                        <h5 className='text-xl text-myBlack dark:text-white'>{notification.name}</h5>
+                                                        <span className='text-lg text-myGrey dark:text-myLine'>{notification.des}</span>
+                                                    </div>
+                                                    <div className='flex flex-wrap justify-between items-center'>
+                                                        <div className='flex items-center gap-1'>
+                                                            <span className='text-myWhite dark:text-myLine'>{notification.time}</span>
+                                                            <div className='w-[6px] h-[6px] bg-[#FFA5A5] rounded-full'></div>
+                                                        </div>
+                                                        <span className='text-xs text-myBlue dark:text-myGreen'>{notification.date}</span>
+                                                    </div>
+                                                </div>
+                                            </div>))
+                                        }
+                                    </div>
+                                </section>
+                            </Tab.Panel>
+                            <Tab.Panel>
+                                <section className='mt-[30px] min-h-[60vh]'>
+                                    <h4 className='text-xl text-myBlack dark:text-white mb-6'>Latest</h4>
+                                    <div>
+                                        {
+                                            latestNotifications.map(notification => (<div
+                                                key={notification.id}
+                                                className='flex flex-wrap gap-3  bg-bgColor dark:bg-[#1E192A] py-5 px-3 mb-2 rounded'
+                                            >
+                                                <Image className='w-[60px] h-[60px] rounded-full' src={notification.img} alt="people" />
+                                                <div>
+                                                    <div className='flex flex-wrap gap-2'>
+                                                        <h5 className='text-xl text-myBlack dark:text-white'>{notification.name}</h5>
+                                                        <span className='text-lg text-myGrey dark:text-myLine'>{notification.des}</span>
+                                                    </div>
+                                                    <div className='flex flex-wrap justify-between items-center'>
+                                                        <div className='flex items-center gap-1'>
+                                                            <span className='text-myBlack dark:text-myLine'>{notification.time}</span>
+                                                            <div className='w-[6px] h-[6px] bg-[#FFA5A5] rounded-full'></div>
+                                                        </div>
+                                                        <span className='text-xs text-myBlue dark:text-myGreen'>{notification.date}</span>
+                                                    </div>
+                                                </div>
+                                            </div>))
+                                        }
+                                    </div>
+                                </section>
+                            </Tab.Panel>
+
+                        </Tab.Panels>
+                    </Tab.Group>
+                </div>
+            </div>
+            <hr className='mt-6 border-myLine dark:border-darkLine' />
+        </div>
+
+    );
+};
+
+export default Notification;
--- src/app/utillity/signup/page.js
+++ src/app/utillity/signup/page.js
@@ -19,9 +19,9 @@ const SignUp = () => {
     }
 
     return (
-        <div className="flex flex-wrap min-h-screen bg-[#ECEEFB] dark:bg-darkBg">
+        <div className="flex flex-wrap bg-[#ECEEFB] dark:bg-darkBg">
 
-            <div className="lg:w-1/2 lg:min-h-screen w-full md:h-[700px] sm:h-[500px] bg-[#4F45B6] dark:bg-gradient-to-r from-authColor to-darkBg relative">
+            <div className="lg:w-1/2  w-full bg-[#4F45B6] dark:bg-gradient-to-r from-authColor to-darkBg relative">
                 <Link href='/' className="flex items-center gap-2 ml-10 mt-10 mb-[60px] cursor-pointer">
                     <Image
                         className="w-[31px] h-[36px]"
@@ -30,36 +30,34 @@ const SignUp = () => {
                     />
                     <h1 className="text-white text-2xl font font-bold">Dashpro</h1>
                 </Link>
-
-                <Image className='w-15 h-15 lg:w-[129px] lg:h-[127px] absolute left-10 top-[172px]' src={warth} alt="circel" />
-                <Image className='w-[129px] h-[127px] absolute right-[67px] top-[133px]' src={warth} alt="circel" />
-                <Image className='w-[72px] h-[71px] absolute right-[34px] bottom-[35px]' src={warth} alt="circel" />
-                <Image className='w-[59px] h-[59px] absolute left-[18%] bottom-[20%]' src={warth} alt="circel" />
-                <Image className='w-[59px] h-[59px] absolute left-[75px] bottom-[64px]' src={warth} alt="circel" />
-
-                <div>
-
+                <div className='flex h-[70vh] items-center justify-center'>
                     <Image
-                        className='centerDiv object-cover'
+                        className=' object-cover'
                         src={animation}
                         alt='animation'
                     />
-                    <div className='absolute bottom-[66px] left-[27%] hidden lg:block '>
-                        <div className='flex flex-wrap gap-[10px] items-center'>
-                            <h4 className='text-2xl text-white font-semibold'>Welcome to</h4>
-                            <div className="flex items-center gap-2">
-                                <Image
-                                    className="w-[31px] h-[36px]"
-                                    src={logo}
-                                    alt="company logo"
-                                />
-                                <h1 className="text-white text-2xl font font-bold">Dashpro</h1>
-                            </div>
-                        </div>
-                        <h5 className='text-xl text-white mt-5'>Start your Dashpro journey now!</h5>
-                    </div>
                 </div>
 
+                <div className='text-center pb-[60px] sm:block hidden'>
+                    <div className='flex flex-wrap gap-[10px] items-center justify-center'>
+                        <h4 className='text-2xl text-white font-semibold'>Welcome to</h4>
+                        <div className="flex items-center gap-2">
+                            <Image
+                                className="w-[31px] h-[36px]"
+                                src={logo}
+                                alt="company logo"
+                            />
+                            <h1 className="text-white text-2xl font-bold">Dashpro</h1>
+                        </div>
+                    </div>
+                    <h5 className='text-xl text-white mt-5'>Start your Dashpro journey now!</h5>
+                </div>
+
+                <Image className='w-15 h-15 lg:w-[129px] lg:h-[127px] absolute left-10 top-[172px] sm:flex hidden' src={warth} alt="circel" />
+                <Image className='w-[129px] h-[127px] absolute right-[67px] top-[133px] sm:flex hidden' src={warth} alt="circel" />
+                <Image className='w-[72px] h-[71px] absolute right-[34px] bottom-[35px]' src={warth} alt="circel" />
+                <Image className='w-[59px] h-[59px] absolute left-[18%] bottom-[20%]' src={warth} alt="circel" />
+                <Image className='w-[59px] h-[59px] absolute left-[75px] bottom-[64px]' src={warth} alt="circel" />
 
                 <Image className='absolute top-[30%] left-[33%] w-[37px] h-[37px] rounded-full run1' src={authFacebook} alt="Facebook" />
                 <Image className='absolute top-[43%] right-[28%] w-[37px] h-[37px] rounded-full run' src={authApple} alt="apple" />
@@ -69,85 +67,87 @@ const SignUp = () => {
             </div>
 
             <div className=" lg:w-1/2 w-full relative cardShadow">
-                <div className='w-full lg:w-4/5 centerDiv bg-white dark:bg-darkCard p-3 lg:px-8 lg:py-7 rounded'>
-                    <h3 className='text-2xl lg:text-[32px] text-myBlack dark:text-white  text-center font-semibold'>Sign In to Dashpro</h3>
-                    <div className='flex flex-wrap justify-center gap-3 my-7'>
-                        <div className='w-10 h-8 px-[10px] py-[6px] bg-[#edecfb] dark:bg-[#274346] rounded cursor-pointer'>
-                            <FaGoogle className='w-5 h-5 text-myBlue dark:text-myGreen' />
+                <div className=' flex h-[100vh] items-center justify-center'>
+                    <div className='w-full lg:w-4/5 bg-white dark:bg-darkCard p-3 lg:px-8 lg:py-7 rounded'>
+                        <h3 className='text-2xl lg:text-[32px] text-myBlack dark:text-white  text-center font-semibold'>Sign In to Dashpro</h3>
+                        <div className='flex flex-wrap justify-center gap-3 my-7'>
+                            <div className='w-10 h-8 px-[10px] py-[6px] bg-[#edecfb] dark:bg-[#274346] rounded cursor-pointer'>
+                                <FaGoogle className='w-5 h-5 text-myBlue dark:text-myGreen' />
+                            </div>
+                            <div className='w-10 h-8 px-[10px] py-[6px] bg-[#edecfb] dark:bg-[#274346] rounded cursor-pointer'>
+                                <FaApple className='w-5 h-5 text-myBlue dark:text-myGreen' />
+                            </div>
+                            <div className='w-10 h-8 px-[10px] py-[6px] bg-[#edecfb] dark:bg-[#274346] rounded cursor-pointer'>
+                                <FaTwitter className='w-5 h-5 text-myBlue dark:text-myGreen' />
+                            </div>
+                            <div className='w-10 h-8 px-[10px] py-[6px] bg-[#edecfb] dark:bg-[#274346] rounded cursor-pointer'>
+                                <FaFacebook className='w-5 h-5 text-myBlue dark:text-myGreen' />
+                            </div>
                         </div>
-                        <div className='w-10 h-8 px-[10px] py-[6px] bg-[#edecfb] dark:bg-[#274346] rounded cursor-pointer'>
-                            <FaApple className='w-5 h-5 text-myBlue dark:text-myGreen' />
-                        </div>
-                        <div className='w-10 h-8 px-[10px] py-[6px] bg-[#edecfb] dark:bg-[#274346] rounded cursor-pointer'>
-                            <FaTwitter className='w-5 h-5 text-myBlue dark:text-myGreen' />
-                        </div>
-                        <div className='w-10 h-8 px-[10px] py-[6px] bg-[#edecfb] dark:bg-[#274346] rounded cursor-pointer'>
-                            <FaFacebook className='w-5 h-5 text-myBlue dark:text-myGreen' />
-                        </div>
+                        <div className="divider dark:text-myWhite">or do it via E-mail</div>
+                        <form>
+                            <div className="form-control w-full mb-5">
+                                <label className="label">
+                                    <span className="text-lg text-myBlack dark:text-white">Full Name</span>
+                                </label>
+                                <input type="text" placeholder="Name..." className="px-[18px] py-2 bg-[#ECEEFB] dark:bg-[#1E192A] text-myBlack dark:text-myWhite outline-none rounded" />
+                            </div>
+                            <div className="form-control w-full mb-5">
+                                <label className="label">
+                                    <span className="text-lg text-myBlack dark:text-white">Email address</span>
+                                </label>
+                                <input type="text" placeholder="info@example.com" className="px-[18px] py-2 bg-[#ECEEFB] dark:bg-[#1E192A] text-myBlack dark:text-myWhite outline-none rounded" />
+                            </div>
+                            <div className="form-control mb-5">
+                                <label className="label">
+                                    <span className="text-lg text-myBlack dark:text-white">Password</span>
+                                </label>
+                                <div className='relative'>
+                                    <input type={show ? 'text' : 'password'} placeholder="Confirm password" className="px-[18px] py-2 text-myBlack dark:text-myWhite bg-[#ECEEFB] dark:bg-[#1E192A] outline-none rounded w-full" />
+                                    <label
+                                        onClick={handleShow}
+                                        className='text-myGrey  absolute right-[3%] top-2 cursor-pointer'>
+                                        {
+                                            show ? <span class="material-symbols-outlined">visibility_off</span> :
+                                                <span class="material-symbols-outlined">visibility</span>
+                                        }
+                                    </label>
+                                </div>
+                            </div>
+                            <div className="form-control mb-5">
+                                <label className="label">
+                                    <span className="text-lg text-myBlack dark:text-white">Password</span>
+                                </label>
+                                <div className='relative'>
+                                    <input type={show ? 'text' : 'password'} placeholder="*******************" className="px-[18px] py-2 text-myBlack dark:text-myWhite bg-[#ECEEFB] dark:bg-[#1E192A] outline-none rounded w-full" />
+                                    <label
+                                        onClick={handleShow}
+                                        className='text-myGrey  absolute right-[3%] top-2 cursor-pointer'>
+                                        {
+                                            show ? <span class="material-symbols-outlined">visibility_off</span> :
+                                                <span class="material-symbols-outlined">visibility</span>
+                                        }
+                                    </label>
+                                </div>
+                                <label className="label">
+                                    <Link href='#' className='text-myGrey dark:text-myWhite text-xs'>Must be 8 characters at least</Link>
+                                </label>
+                            </div>
+                            <div className='flex flex-wrap justify-between items-center'>
+                                <div className="form-control">
+                                    <label className="flex items-center gap-[10px] cursor-pointer">
+                                        <input type="checkbox" className="checkbox checkbox-primary dark:checkbox-success" />
+                                        <span className="text-xl text-myBlack dark:text-myWhite">Remember me</span>
+                                    </label>
+                                </div>
+                                <Link href="#" className='text-myBlue dark:text-myGreen text-lg'>Forgot Password?</Link>
+                            </div>
+                            <div className="mt-10 w-4/5 mx-auto">
+                                <button className="w-full px-4 py-3 text-sm bg-myBlue dark:bg-myGreen text-white dark:text-[#1E192A] rounded">Register</button>
+                            </div>
+                            <span className='text-myBlack dark:text-myWhite block text-center text-lg mt-3'>Already have an account? <Link href='/utillity/signin' className='text-myBlue dark:text-myGreen'>Login</Link></span>
+                        </form>
                     </div>
-                    <div className="divider dark:text-myWhite">or do it via E-mail</div>
-                    <form>
-                        <div className="form-control w-full mb-5">
-                            <label className="label">
-                                <span className="text-lg text-myBlack dark:text-white">Full Name</span>
-                            </label>
-                            <input type="text" placeholder="Name..." className="px-[18px] py-2 bg-[#ECEEFB] dark:bg-[#1E192A] text-myBlack dark:text-myWhite outline-none rounded" />
-                        </div>
-                        <div className="form-control w-full mb-5">
-                            <label className="label">
-                                <span className="text-lg text-myBlack dark:text-white">Email address</span>
-                            </label>
-                            <input type="text" placeholder="info@example.com" className="px-[18px] py-2 bg-[#ECEEFB] dark:bg-[#1E192A] text-myBlack dark:text-myWhite outline-none rounded" />
-                        </div>
-                        <div className="form-control mb-5">
-                            <label className="label">
-                                <span className="text-lg text-myBlack dark:text-white">Password</span>
-                            </label>
-                            <div className='relative'>
-                                <input type={show ? 'text' : 'password'} placeholder="Confirm password" className="px-[18px] py-2 text-myBlack dark:text-myWhite bg-[#ECEEFB] dark:bg-[#1E192A] outline-none rounded w-full" />
-                                <label
-                                    onClick={handleShow}
-                                    className='text-myGrey  absolute right-[3%] top-2 cursor-pointer'>
-                                    {
-                                        show ? <span class="material-symbols-outlined">visibility_off</span> :
-                                            <span class="material-symbols-outlined">visibility</span>
-                                    }
-                                </label>
-                            </div>
-                        </div>
-                        <div className="form-control mb-5">
-                            <label className="label">
-                                <span className="text-lg text-myBlack dark:text-white">Password</span>
-                            </label>
-                            <div className='relative'>
-                                <input type={show ? 'text' : 'password'} placeholder="*******************" className="px-[18px] py-2 text-myBlack dark:text-myWhite bg-[#ECEEFB] dark:bg-[#1E192A] outline-none rounded w-full" />
-                                <label
-                                    onClick={handleShow}
-                                    className='text-myGrey  absolute right-[3%] top-2 cursor-pointer'>
-                                    {
-                                        show ? <span class="material-symbols-outlined">visibility_off</span> :
-                                            <span class="material-symbols-outlined">visibility</span>
-                                    }
-                                </label>
-                            </div>
-                            <label className="label">
-                                <Link href='#' className='text-myGrey dark:text-myWhite text-xs'>Must be 8 characters at least</Link>
-                            </label>
-                        </div>
-                        <div className='flex flex-wrap justify-between items-center'>
-                            <div className="form-control">
-                                <label className="flex items-center gap-[10px] cursor-pointer">
-                                    <input type="checkbox" className="checkbox checkbox-primary dark:checkbox-success" />
-                                    <span className="text-xl text-myBlack dark:text-myWhite">Remember me</span>
-                                </label>
-                            </div>
-                            <Link href="#" className='text-myBlue dark:text-myGreen text-lg'>Forgot Password?</Link>
-                        </div>
-                        <div className="mt-10 w-4/5 mx-auto">
-                            <button className="w-full px-4 py-3 text-sm bg-myBlue dark:bg-myGreen text-white dark:text-[#1E192A] rounded">Register</button>
-                        </div>
-                        <span className='text-myBlack dark:text-myWhite block text-center text-lg mt-3'>Already have an account? <Link href='/utillity/signin' className='text-myBlue dark:text-myGreen'>Login</Link></span>
-                    </form>
                 </div>
             </div>
         </div>
